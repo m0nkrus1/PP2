989e33311826d7fd628dae6c7a20398c99788145
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-01-19 23:56:51 +0500

    Adding datatypes

---
+++ Lab1/data_types.py
@@ -0,0 +1,22 @@
+x = "Hello World"
+print(x)
+
+#display the data type of x:
+print(type(x)) #<class 'str'>
+
+x = ["apple", "banana", "cherry"]
+
+print(x)
+
+#display the data type of x:
+print(type(x)) #<class 'list'>
+
+x = range(6)
+
+print(x)
+
+#display the data type of x:
+print(type(x)) #<class 'range'>
+
+
+
