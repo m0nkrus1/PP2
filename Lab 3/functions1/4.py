2ec72a376aed41f65db33d64443afdf86b863719
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-02-02 19:08:59 +0500

    added 4.py

---
+++ Lab 3/functions1/4.py
@@ -0,0 +1,15 @@
+# 4 
+# function that checks if the number prime or not 
+def is_prime(n): 
+    if n < 2: 
+        return False 
+    for i in range(2, int(n ** 0.5) + 1): 
+        if n % i == 0: 
+            return False 
+    return True 
+ 
+def filter_prime(numbers): 
+    return [num for num in numbers if is_prime(num)] 
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split())) 
+print("Prime numbers:", filter_prime(numbers)) 
+ 
