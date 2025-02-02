a9468154c40581c75d021744019aa83852b1fb7a
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-02-02 17:49:21 +0500

    added 8.py

---
+++ Lab 3/functions1/8.py
@@ -0,0 +1,8 @@
+def spy_game(nums):
+    for i in range(len(nums) - 2):
+        if nums[i] == 0 and nums[i+1] == 0 and nums[i+2] == 7:
+            return True
+    return False
+spy_game([1,2,4,0,0,7,5]) # True
+spy_game([1,0,2,4,0,5,7]) # True
+spy_game([1,7,2,0,4,5,0]) # False
