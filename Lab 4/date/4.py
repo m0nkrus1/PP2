724f64502e07948cab02887cc22c617e3c5b9bf8
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-02-16 00:22:25 +0500

    added 4.py

---
+++ Lab 4/date/4.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+time1 = input("Enter the 1st date (YYYY-MM-DD HH:MM:SS): ")
+time2 = input("Enter the 2nd date (YYYY-MM-DD HH:MM:SS): ")
+date1 = datetime.strptime(time1, "%Y-%m-%d %H:%M:%S")
+date2 = datetime.strptime(time2, "%Y-%m-%d %H:%M:%S")
+diff = abs(date1 - date2)
+seconds_diff = diff.total_seconds()
+print(seconds_diff)
