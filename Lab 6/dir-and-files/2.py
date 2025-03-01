3c322f611856053349a7f541d4b4b859d25bb4fd
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-03-02 00:04:39 +0500

    2.py

---
+++ Lab 6/dir-and-files/2.py
@@ -0,0 +1,6 @@
+import os
+path = r"C:\Users\Aldaniyazov\Desktop\PP2"  
+print("Exists:", os.path.exists(path))
+print("Readable:", os.access(path, os.R_OK))
+print("Writable:", os.access(path, os.W_OK))
+print("Executable:", os.access(path, os.X_OK))
