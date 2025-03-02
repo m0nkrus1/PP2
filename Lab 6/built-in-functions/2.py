772f8e559efc69f9f90c78f6f79dc41e14b06657
Aldaniyazov <rauanaldaniazov@gmail.com>
2025-03-02 20:16:59 +0500

    2.py

---
+++ Lab 6/built-in-functions/2.py
@@ -0,0 +1,10 @@
+st =input("Enter a string:")
+upper1 = 0
+lower1 = 0
+for i in st:
+    if i.isupper():
+        upper1+=1
+    if i.islower():
+        lower1+=1
+print('lower:', lower1)
+print('upper', upper1)
